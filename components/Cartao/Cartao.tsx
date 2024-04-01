6a1410e0cf96246e715787ca21cab4199986ed4b
marcelo.alves <marcelo.alves@pt.clara.net>
2024-04-01 12:03:46 +0100

    changes

--- components/Cartao/Cartao.tsx
+++ components/Cartao/Cartao.tsx
@@ -8,24 +8,59 @@ interface CartaoProps {
    * The name of the person
    */
   name: string;
-    /**
-     * The photo of the person
-     */
+  /**
+   * The photo of the person
+   */
   photo: string;
 }
 
 export default function Cartao({ name, photo }: CartaoProps) {
   return (
     <div className="cartao">
-      <Image
+      <div className="cartao">
+        <div className="card-data grid justify-center gap-32 p-32">
+          <div
+            className="frente"
+            style={{
+              backgroundImage: `url(${ResidenceFrente.src})`,
+              backgroundPosition: "center",
+              backgroundSize: "cover",
+              backgroundRepeat: "no-repeat",
+              width: "500px",
+              height: "315px",
+            }}
+          >
+            <Image
+              src={photo || "https://via.placeholder.com/150"}
+              alt="card image photo"
+              className="photo"
+              width={150}
+              height={150}
+            />
+            <span className="name">{name}</span>
+          </div>
+          <div
+            className="frente"
+            style={{
+              backgroundImage: `url(${ResidenceVerso.src})`,
+              backgroundPosition: "center",
+              backgroundSize: "cover",
+              backgroundRepeat: "no-repeat",
+              width: "500px",
+              height: "315px",
+            }}
+          ></div>
+        </div>
+      </div>
+      {/* <Image
         src={photo || "https://via.placeholder.com/150"}
         alt="card image photo"
         className="photo"
         width={150}
         height={150}
       />
-      <span className="name">{name}</span>
-      <Image
+      <span className="name">{name}</span> */}
+      {/* <Image
         src={ResidenceFrente}
         alt="residence permit"
         width={500}
@@ -37,7 +72,7 @@ export default function Cartao({ name, photo }: CartaoProps) {
         alt="residence permit"
         width={500}
         height={16}
-      />
+      /> */}
     </div>
   );
 }
--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['via.placeholder.com'],
+      remotePatterns: [
+        {
+          protocol: "https",
+          hostname: "**",
+        },
+      ],
     },
-};
+  };
 
 export default nextConfig;
